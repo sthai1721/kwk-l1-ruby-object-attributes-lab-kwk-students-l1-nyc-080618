cd5c648ba51b9cf99ecfe2bf6aa4daf474e2da39
sthai1721 <sophia.thai@rutgers.edu>
2018-08-09 19:04:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+
+class Dog
+  def name=(new_name)
+    @name=new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(breed_name)
+    @breed=breed_name
+  end
+  
+  def breed
+    @breed
+  end
+end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # person.rb
+
+class Person
+  def name=(new_name)
+    @name=new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_name)
+    @job=job_name
+  end
+  
+  def job
+    @job
+  end
+end
